ff28974290587941db5bd31f9d6068cd6768be59
Amar Zavery <amzavery@microsoft.com>
2018-04-25 09:55:45 -0700

    minor tweaks to the node.js script

--- test/receiver-node/index.js
+++ test/receiver-node/index.js
@@ -1,35 +1,68 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 require('dotenv').config()
 
-const { EventHubClient, EventPosition } = require('azure-event-hubs')
+const { EventHubClient, EventPosition } = require('azure-event-hubs');
+const partitionCount = {};
 
-const namespace_name = process.env["EVENTHUB_NAMESPACE_NAME"]
-const saspolicy_name = process.env["EVENTHUB_KEY_NAME"]
-const saspolicy_secret = process.env["EVENTHUB_KEY_VALUE"]
-const hub_name = process.env["EVENTHUB_HUB_NAME"]
-
-const conn_string = `Endpoint=sb://${namespace_name}.servicebus.windows.net/;SharedAccessKeyName=${saspolicy_name};SharedAccessKey=${saspolicy_secret}`
-
-function main() {
-    var ehc = EventHubClient.createFromConnectionString(conn_string, hub_name);
-	console.log('Going to listen for events, CTRL+C to exit')
-
-    ehc.getPartitionIds().then((partitionIds) => {
-		for (const partitionId of partitionIds) {
-			console.log(`Listening on: partitionId ${partitionId}, namespace ${namespace_name}, hub ${hub_name}`)
-			ehc.receiveOnMessage(
-				partitionId,
-				(eventData) => { // onMessage
-					console.log(eventData.body.content.toString('utf8'));
-				},
-				(err) => { // onError
-					console.log("An error occurred on the receiver ", err);
-				},
-				{ // options
-					eventPosition: EventPosition.fromEnqueuedTime(Date.now())
-				}
-			)
-		}
-	})
+async function main() {
+  const { namespaceName, saslpolicyName, saslpolicySecret, hubName } = createConfig();
+  const connectionString = `Endpoint=sb://${namespaceName}.servicebus.windows.net/;` +
+    `SharedAccessKeyName=${saslpolicyName};SharedAccessKey=${saslpolicySecret}`
+  const client = EventHubClient.createFromConnectionString(connectionString, hubName);
+  console.log('Going to listen for events, CTRL+C to exit')
+  const partitionIds = await client.getPartitionIds();
+  for (const partitionId of partitionIds) {
+    console.log(`Listening on: partitionId ${partitionId}, namespace ${namespaceName}, hub ${hubName}`);
+    partitionCount[partitionId] = 0;
+    const onMessage = (eventData) => {
+      processEvent(eventData, partitionId);
+    };
+    const onError = (err) => {
+      console.log("An error occurred on the receiver ", err);
+    };
+    const options = { eventPosition: EventPosition.fromStart() };
+    // const options = { eventPosition: EventPosition.fromEnqueuedTime(Date.now()) };
+    const receiveHandler = client.receive(partitionId, onMessage, onError, options);
+  }
 }
 
-main()
+main().catch((err) => {
+  console.log("An error occurred while receiving messages: ", err);
+});
+
+// Helper methods
+
+function createConfig() {
+  return {
+    namespaceName: process.env["EVENTHUB_NAMESPACE_NAME"],
+    saslpolicyName: process.env["EVENTHUB_KEY_NAME"],
+    saslpolicySecret: process.env["EVENTHUB_KEY_VALUE"],
+    hubName: process.env["EVENTHUB_HUB_NAME"]
+  };
+}
+
+function processEvent(eventData, partitionId) {
+  console.log(">>>>> Received message from partition id: %d, count: %d",
+    partitionId, ++partitionCount[partitionId]);
+  if (typeof eventData.body === "string")
+    console.log(eventData.body);
+  else if (eventData.body.content)
+    console.log(eventData.body.content.toString("utf8"));
+  else if (Buffer.isBuffer(eventData.body))
+    console.log(eventData.body.toString("utf8"));
+}
+
+const CtrlC = require("death");
+CtrlC((signal, err) => {
+  console.log("\nstats:");
+  console.log("--------------------------------------");
+  console.log(" PartitionId | Received Message Count ");
+  console.log("--------------------------------------");
+  for (key in partitionCount) {
+    console.log(`      ${key}      |          ${partitionCount[key]}`);
+  }
+  console.log("---------------------------------------");
+  process.exit();
+});
